54bbb258c672b59e71a240afbab233b2d07dcf9d
khashab2 <khashab2@illinois.edu>
2015-10-21 21:48:42 -0500

    fixing the issue with model directory

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/spamClassifiers.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/spamClassifiers.scala
@@ -1,12 +1,15 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam
 
+import edu.illinois.cs.cogcomp.lbjava.learn.SparseAveragedPerceptron
 import edu.illinois.cs.cogcomp.saul.datamodel.property.Property
 import edu.illinois.cs.cogcomp.saulexamples.data.Document
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam.spamDataModel._
 import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 
 object spamClassifiers {
-  object spamClassifier extends Learnable[Document](spamDataModel) {
+  val parameters = new SparseAveragedPerceptron.Parameters()
+  parameters.modelDir = "model"
+  object spamClassifier extends Learnable[Document](spamDataModel, parameters) {
 
     def label: Property[Document] = spamLabel
     override def algorithm = "SparseNetwork"
