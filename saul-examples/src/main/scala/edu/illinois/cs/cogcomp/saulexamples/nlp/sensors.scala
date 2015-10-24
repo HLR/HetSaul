5def648666f1948f544f08042c6ef1ff1e9ac4c9
khashab2 <khashab2@illinois.edu>
2015-10-23 22:19:34 -0500

    cleaning up the sensors

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/sensors.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/sensors.scala
@@ -10,49 +10,41 @@ import edu.illinois.cs.cogcomp.saulexamples.data.Document
 
 import scala.collection.JavaConversions._
 
+/**  an object containing many popular sensors used in examples */
 object sensors {
-
-  def getText(x: Document): String = {
-    val words = x.getWords
-    words.mkString(" ")
-  }
 
   def textCollection(x: List[Document]) = {
     x.map(documentContent)
-  }
-
-  def processDocumentWith(annotatorService: AnnotatorService, cid: String, did: String, text: String, services: String*): TextAnnotation = {
-
-    val ta = annotatorService.createBasicTextAnnotation(cid, did, text)
-    // annotatorService.addView(ta, ViewNames.POS)
-    println(ta.getAvailableViews)
-    //The following lines can be used to get classes of various views from TextAnnotation
-    //annotatorService.addView(ta, ViewNames.SHALLOW_PARSE)
-    //val chunks2: SpanLabelView = ta.getView(ViewNames.SHALLOW_PARSE).asInstanceOf[SpanLabelView]
-    //val parse2: SpanLabelView= ta.getView(ViewNames.NER).asInstanceOf[SpanLabelView]
-    //val Pos1: TokenLabelView =ta.getView(ViewNames.POS).asInstanceOf[TokenLabelView]
-    //Add views we need
-    ta
   }
 
   def documentContent(x: Document): String = {
     x.getWords.mkString(" ")
   }
 
-  def f(x: TextAnnotation): List[Sentence] = x.sentences().toList
+  def processDocumentWith(annotatorService: AnnotatorService, cid: String, did: String, text: String, services: String*): TextAnnotation = {
+    val ta = annotatorService.createBasicTextAnnotation(cid, did, text)
+    println(ta.getAvailableViews)
+    ta
+  }
 
-  def alignment(x: TextAnnotation, y: Sentence): Boolean = x.getId == y.getSentenceConstituent.getTextAnnotation.getId
+  def getSentences(x: TextAnnotation): List[Sentence] = {
+    x.sentences().toList
+  }
 
-  def f2(x: TextAnnotation): List[Constituent] = x.getView(ViewNames.POS).getConstituents.toList
+  def sentenceTextAnnotationAlignment(ta: TextAnnotation, sentence: Sentence): Boolean = {
+    ta.getId == sentence.getSentenceConstituent.getTextAnnotation.getId
+  }
 
-  def curator(dat: Document): TextAnnotation = {
+  def getConstituents(x: TextAnnotation): List[Constituent] = {
+    x.getView(ViewNames.POS).getConstituents.toList
+  }
+
+  def annotateWithCurator(document: Document): TextAnnotation = {
     val config = "./saul-examples/config/caching-curator.properties"
     val rm = new ResourceManager(config)
     val annotatorService = CuratorFactory.buildCuratorClient(rm)
-    val a = documentContent(dat)
-    val ret = processDocumentWith(annotatorService, "corpus", dat.getGUID, a)
-    // annotatorService.closeCache()
-    ret
+    val content = documentContent(document)
+    processDocumentWith(annotatorService, "corpus", document.getGUID, content)
   }
 
   def cityGazetSensor: GazeteerReader = {
@@ -65,3 +57,4 @@ object sensors {
     persongazet
   }
 }
+
