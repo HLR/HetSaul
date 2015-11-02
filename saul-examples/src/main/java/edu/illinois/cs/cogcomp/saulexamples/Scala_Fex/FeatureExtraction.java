10fe5b62fea324f54e21480d68362598c4be1080
kordjam <kordjam@illinois.edu>
2015-11-01 18:20:37 -0600

    using scala query results in java: simple initial example.

---
+++ saul-examples/src/main/java/edu/illinois/cs/cogcomp/saulexamples/Scala_Fex/FeatureExtraction.java
@@ -0,0 +1,12 @@
+package edu.illinois.cs.cogcomp.saulexamples.Scala_Fex;
+
+import edu.illinois.cs.cogcomp.saulexamples.nlp.DataModelTests.graphQueries;
+/**
+ * Created by Parisa on 11/1/15.
+ */
+public class FeatureExtraction {
+    public static void main(String[] args) {
+        System.out.print("it works!");
+        System.out.print(graphQueries.f());
+    }
+}
