d5d818518939fe15149c60d55b03d60535890028
kordjam <kordjam@illinois.edu>
2015-11-01 18:26:31 -0600

    format

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/DataModelTests/GraphQueries.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/DataModelTests/GraphQueries.scala
@@ -16,7 +16,8 @@ object graphQueries extends App {
   }
 
   import model._
-  val f= ((token() ~> t2s).node == sentence) // this can be called from a java program
+  val f = ((token() ~> t2s).node == sentence) // this can be called from a java program
+  print(":" + f)
   println((token() ~> t2s).node == sentence)
   println((token() ~> t2s).node == sentence)
   println((token() ~> t2s ~> -t2s).node == token)
