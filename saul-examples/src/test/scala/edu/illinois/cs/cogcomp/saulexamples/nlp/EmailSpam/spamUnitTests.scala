1bbe9e4f8778255f56408855811f5a131f7db4c6
khashab2 <khashab2@illinois.edu>
2015-11-03 14:21:25 -0600

    minor change to classifier uTest.

--- saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/spamUnitTests.scala
+++ saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/spamUnitTests.scala
@@ -39,7 +39,7 @@ class SpamUnitTests extends FlatSpec with Matchers {
   }
 
   "classifier " should "overfit" in {
-    val trainData = toyDataGenerator.generateToyDocuments(1)
+    val trainData = toyDataGenerator.generateToyDocumentsSingleLabel(10)
     docs populate trainData
     spamClassifier.learn(30)
     spamClassifier.classifier.discreteValue(trainData.head) should be(trainData.head.getLabel)
@@ -51,6 +51,11 @@ object toyDataGenerator {
   val documentString = "Saul or Soul; that is the question"
   def generateToyDocuments(numDocs: Int): IndexedSeq[Document] = {
     (1 to numDocs).map(_ => new Document(documentString.split(" ").toList, util.Random.nextInt(2).toString))
+  }
+
+  def generateToyDocumentsSingleLabel(numDocs: Int): IndexedSeq[Document] = {
+    val label = util.Random.nextInt(2).toString
+    (1 to numDocs).map(_ => new Document(documentString.split(" ").toList, label))
   }
 
   def generateToyTextAnnotation(numDocs: Int): IndexedSeq[TextAnnotation] = {
