ce85aa5ba7580ec31e5d7b2e5c1e728726a9e753
kordjam <kordjam@illinois.edu>
2015-11-04 13:52:19 -0600

    example for calling Saul queries from Java

--- saul-examples/src/main/java/edu/illinois/cs/cogcomp/saulexamples/Scala_Fex/FeatureExtraction.java
+++ saul-examples/src/main/java/edu/illinois/cs/cogcomp/saulexamples/Scala_Fex/FeatureExtraction.java
@@ -1,13 +1,14 @@
 package edu.illinois.cs.cogcomp.saulexamples.Scala_Fex;
 
-import edu.illinois.cs.cogcomp.saulexamples.nlp.DataModelTests.graphQueries;
-/**
- * Created by Parisa on 11/1/15.
- */
+import edu.illinois.cs.cogcomp.saulexamples.nlp.DataModelTests.graphForFex;
+
+
+
 public class FeatureExtraction {
     public static void main(String[] args) {
-        System.out.print("it works!");
-       // new GraphQueriesTest();
-        System.out.print(graphQueries.f());
-    }
+        System.out.println("These queries are called from Saul's datamodel!");
+        if (graphForFex.query2().iterator().hasNext())
+        System.out.println("Prefix:" + graphForFex.query2().iterator().next());
+        System.out.println("Prefix:" + graphForFex.query1());
+}
 }
