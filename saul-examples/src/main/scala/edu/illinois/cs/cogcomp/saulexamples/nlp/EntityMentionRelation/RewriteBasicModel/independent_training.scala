2f70e48a5829ae5f900233c469ac29cf0cfe8bbc
kordjam <kordjam@illinois.edu>
2015-11-20 08:23:10 -0600

    formatting

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/RewriteBasicModel/independent_training.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/RewriteBasicModel/independent_training.scala
@@ -1,13 +1,13 @@
-  package edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.RewriteBasicModel
+package edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.RewriteBasicModel
 
-  import edu.illinois.cs.cogcomp.saul.classifier.Learnable
+import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 
-  object independent_training {
-  def apply[T<:AnyRef](c:List[Learnable[T]])=
-  c.foreach((x:Learnable[T])=>x.learn(10))
-  }
+object independent_training {
+  def apply[T <: AnyRef](c: List[Learnable[T]]) =
+    c.foreach((x: Learnable[T]) => x.learn(10))
+}
 
-  object independent_test {
-    def apply[T<:AnyRef](c:List[Learnable[T]])=
-      c.foreach((x:Learnable[T])=>x.test())
-  }
+object independent_test {
+  def apply[T <: AnyRef](c: List[Learnable[T]]) =
+    c.foreach((x: Learnable[T]) => x.test())
+}
