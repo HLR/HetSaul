040672a00d4cafa224d364a4dbb26496dee4b797
kordjam <kordjam@illinois.edu>
2015-11-20 06:48:56 -0600

    simplify compose learning functions,independent case.

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/RewriteBasicModel/IndenpendentTraining.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/RewriteBasicModel/IndenpendentTraining.scala
@@ -6,7 +6,8 @@ import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.RewriteBas
 
 import scala.collection.JavaConversions._
 
-object indenpendentTraining extends App {
+
+object IndenpendentTraining extends App {
 
   def populate_ER_graph = {
     val reader = new Conll04_ReaderNew("./data/EntityMentionRelation/conll04.corp", "Token")
@@ -17,24 +18,13 @@ object indenpendentTraining extends App {
     sentences populate trainSentences
     tokens populate trainTokens
     pairs populate trainRelations
+
     testWith(trainTokens)
     testWith(trainRelations)
     testWith(trainSentences)
-  }
+ }
 
   populate_ER_graph
-
-  val it = 2
-  println("Indepent Training with iteration " + it)
-  personClassifier.learn(it)
-  personClassifier.test(tokens.getAllInstances)
-  orgClassifier.learn(it)
-  orgClassifier.test(tokens.getAllInstances)
-  locationClassifier.learn(it)
-  locationClassifier.test(tokens.getAllInstances)
-  //  workForClassifier.learn(it)
-  //  workForClassifier.test()
-  //  LivesInClassifier.learn(it)
-  //  LivesInClassifier.test()
-
+  independent_training(List(personClassifier, orgClassifier, locationClassifier))
+  independent_test(List(personClassifier, orgClassifier, locationClassifier))
 }
