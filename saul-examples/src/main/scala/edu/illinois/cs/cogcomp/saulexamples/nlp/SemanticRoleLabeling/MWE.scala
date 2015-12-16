5d3e0bbbea2ecd0c23077a8dce7512119090ea10
kordjam <kordjam@illinois.edu>
2015-12-16 12:37:21 -0600

    MWE for SRL debug

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/MWE.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/MWE.scala
@@ -1,8 +1,10 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling
 
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames
-import edu.illinois.cs.cogcomp.core.datastructures.textannotation.{ Constituent, Relation }
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.{Constituent, Relation}
 import edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling.SRLDataModel._
+import scala.collection.JavaConversions._
+
 /** Created by Parisa on 12/15/15.
   */
 object MWE extends App {
@@ -11,10 +13,18 @@ object MWE extends App {
   // Generate predicate candidates by extracting all verb tokens
   val predicateCandidates = tokens().filter((x: Constituent) => posTag(x).startsWith("VB"))
     .map(c => c.cloneForNewView(ViewNames.SRL_VERB))
-  predicates.populate(predicateCandidates)
+
+  val negativePredicateCandidates = predicates(predicateCandidates)
+    .filterNot(cand => (predicates() prop address).contains(address(cand)))
+
+  predicates.populate(negativePredicateCandidates)
 
   val argumentCandidates = tokens().filter((x: Constituent) => posTag(x).startsWith("NN"))
     .map(c => c.cloneForNewView(ViewNames.SRL_VERB))
+
+//  val negativeArgumentCandidates = arguments(argumentCandidates)
+//    .filterNot(cand => (arguments() prop address).contains((arguments(cand) prop address).head))
+
   arguments.populate(argumentCandidates)
 
   //  generate all candidate relations based on candidate arguments and predicates
@@ -24,4 +34,6 @@ object MWE extends App {
   } yield new Relation("candidate", x, y, 0.0)
 
   relations.populate(relationCandidates2)
+
+  print("bye!")
 }
