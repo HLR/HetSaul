132023eec9a208b0d72cd2921189a459bb122052
kordjam <kordjam@illinois.edu>
2015-12-25 16:49:05 -0600

    added nonOverlaping constraint

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/sRLConstraints.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/sRLConstraints.scala
@@ -1,17 +1,18 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling
 
-import edu.illinois.cs.cogcomp.core.datastructures.textannotation.{ Relation, TextAnnotation }
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.{Constituent, TokenLabelView, Relation, TextAnnotation}
 import edu.illinois.cs.cogcomp.lbjava.infer.{ FirstOrderConstant, FirstOrderConstraint }
 import edu.illinois.cs.cogcomp.saul.classifier.ConstrainedClassifier
 import edu.illinois.cs.cogcomp.saul.constraint.ConstraintTypeConversion._
 import edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling.SRLClassifiers.argumentTypeLearner
 import edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling.SRLDataModel._
 import edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling.relationAppXuPalmerCandidates._
-
+import scala.collection.JavaConversions._
 /** Created by Parisa on 12/23/15.
   */
 object sRLConstraints {
-  val noOverlap = ConstrainedClassifier.constraintOf[TextAnnotation]( // here tis constituent is a sentence
+  val noOverlap = ConstrainedClassifier.constraintOf[TextAnnotation]( // here this constituent is a sentence
 
     {
       var a: FirstOrderConstraint = new FirstOrderConstant(true)
@@ -19,39 +20,19 @@ object sRLConstraints {
         (sentences(x) ~> sentencesToRelations ~> relationsToPredicates).foreach {
           y =>
             {
-              a = Xucandisates(y).toList._atMost(1)({
-                (p: Relation) =>
-                  (argumentTypeLearner on p isTrue)
-              })
+              val argCandList= Xucandisates(y)
+
+              x.getView(ViewNames.TOKENS).asInstanceOf[TokenLabelView].getConstituents.toList.foreach{
+                t: Constituent =>
+                  val contains= argCandList.filter( x => x.getTarget.doesConstituentCover(t))
+                  a= a &&& (contains.toList._atMost(1)
+                  ({p: Relation =>  (argumentTypeLearner on p).is("none")unary_! } ))
+              }
             }
         }
-        // on the global object TextAnnoatation
       }
       a
     }
   )
-
-  // atmost 1 of (Argument a in containsWord)
-  // 16. ArgumentTypeLearner(a) !: "null
-
-  //(int i = 0; i < sentence.verbCount(); ++i)
-  ////      3. ParseTreeWord verb = sentence.getVerb(i);
-  //      4. LinkedList forVerb = sentence.getCandidates(verb);
-  //      5. for (int j = 0; j < sentence.words.length; ++j) {
-  //        6. if (sentence.words[j] == verb) continue;
-  //        7. LinkedList containsWord = new LinkedList();
-  //        8. for (Iterator I = forVerb.iterator(); I.hasNext(); ) {
-  //          9. Argument candidate = (Argument) I.next();
-  //          10. ParseTreeNode constituent = candidate.getConstituent();
-  //          11. if (constituent.firstWordIndex() <= sentence.words[j].index
-  //            12. && sentence.words[j].index <= constituent.lastWordIndex())
-  //          13. containsWord.add(candidate);
-  //          14. }
-  //        15. atmost 1 of (Argument a in containsWord)
-  //        16. ArgumentTypeLearner(a) !: "null";
-  //        17. }
-  //      18. }
-  //    19. }
-
 }
 
