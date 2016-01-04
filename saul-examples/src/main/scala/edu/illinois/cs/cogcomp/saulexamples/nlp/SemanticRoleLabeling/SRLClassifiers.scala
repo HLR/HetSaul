b047bf4b715f7317fb96facea624d3901e12704d
khashab2 <khashab2@illinois.edu>
2016-01-04 01:33:33 -0600

    updating Lernable.scala

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/SRLClassifiers.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/SRLClassifiers.scala
@@ -1,7 +1,7 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling
 
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.{ Relation, Constituent }
-import edu.illinois.cs.cogcomp.lbjava.learn.SparseAveragedPerceptron
+import edu.illinois.cs.cogcomp.lbjava.learn.{ SparseNetworkLearner, SparseAveragedPerceptron }
 import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 import edu.illinois.cs.cogcomp.saul.datamodel.property.Property
 
@@ -10,22 +10,23 @@ object SRLClassifiers {
   val parameters = new SparseAveragedPerceptron.Parameters()
   //  parameters.modelDir = new ExamplesConfigurator().getDefaultConfig.getString(ExamplesConfigurator.MODELS_DIR.getFirst)
   object predicateClassifier extends Learnable[Constituent](SRLDataModel, parameters) {
-
     //TODO These are not used during Learner's initialization
     def label: Property[Constituent] = SRLDataModel.isPredicate
-    override def algorithm = "SparseNetwork"
+    override lazy val classifier = new SparseNetworkLearner()
   }
 
   object argumentClassifier extends Learnable[Constituent](SRLDataModel, parameters) {
     def label = SRLDataModel.isArgument
+    override lazy val classifier = new SparseNetworkLearner()
   }
 
   object predicateSenseClassifier extends Learnable[Constituent](SRLDataModel, parameters) {
     def label = SRLDataModel.predicateSense
+    override lazy val classifier = new SparseNetworkLearner()
   }
 
   object relationClassifier extends Learnable[Relation](SRLDataModel, parameters) {
     def label = SRLDataModel.argumentLabel
+    override lazy val classifier = new SparseNetworkLearner()
   }
-
 }
--- saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/LBJavaSerializationTest.scala
+++ saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/LBJavaSerializationTest.scala
@@ -1,12 +1,14 @@
 package edu.illinois.cs.cogcomp.saulexamples
 
+import edu.illinois.cs.cogcomp.lbjava.learn.SparseNetworkLearner
 import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 import edu.illinois.cs.cogcomp.saul.constraint.ConstraintTypeConversion._
 import edu.illinois.cs.cogcomp.saulexamples.data.Document
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam.spamClassifiers.spamClassifier
+import edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam.spamClassifiers.spamClassifier._
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam.toyDataGenerator
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{ Matchers, FlatSpec }
 
 class LBJavaSerializationTest extends FlatSpec with Matchers {
 
@@ -18,20 +20,20 @@ class LBJavaSerializationTest extends FlatSpec with Matchers {
 
   spamDataModel.docs populate trainData
   spamClassifier.learn(10)
-  val predictionsBeforeSerialization = testData.map( spamClassifier.classifier.discreteValue(_))
+  val predictionsBeforeSerialization = testData.map(spamClassifier.classifier.discreteValue(_))
 
   spamClassifier.classifier.save()
 
   object deserializedSpamClassifier extends Learnable[Document](spamDataModel) {
     def label = spamLabel
-    override def algorithm = "SparseNetwork"
+    override lazy val classifier = new SparseNetworkLearner()
     override def feature = using(wordFeature)
   }
 
-  val predictionsAfterSerialization = testData.map( deserializedSpamClassifier.classifier.discreteValue(_))
+  val predictionsAfterSerialization = testData.map(deserializedSpamClassifier.classifier.discreteValue(_))
 
   /** making sure that serialization is working the way it is supposed to be */
   "LBJava serialization " should " work " in {
-    predictionsAfterSerialization.indices.foreach( it => predictionsAfterSerialization(it) should be(predictionsBeforeSerialization(it)) )
+    predictionsAfterSerialization.indices.foreach(it => predictionsAfterSerialization(it) should be(predictionsBeforeSerialization(it)))
   }
 }
