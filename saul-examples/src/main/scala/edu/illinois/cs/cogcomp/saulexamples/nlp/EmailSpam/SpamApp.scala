e22bf5d8a60d2eb6fddf23b0ad0487c1e07baa59
khashab2 <khashab2@illinois.edu>
2016-01-09 18:26:25 -0600

    adding a comment

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/SpamApp.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/SpamApp.scala
@@ -16,6 +16,7 @@ object SpamApp {
   }
 
   def main(args: Array[String]): Unit = {
+    /** Choose the experiment you're interested in by changing the following line */
     val testType = SpamExperimentType.TrainAndTest
 
     testType match {
