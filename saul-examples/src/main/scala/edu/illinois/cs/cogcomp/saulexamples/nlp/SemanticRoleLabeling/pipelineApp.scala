89e6214d9b215dc07ff17246b6c4a8d9ab680405
kordjam <kordjam@illinois.edu>
2016-01-14 11:09:02 -0600

    -pipeline is verbosed with all model variations   -for considering gold predicates or gold argument boundaries

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/pipelineApp.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/SemanticRoleLabeling/pipelineApp.scala
@@ -1,6 +1,6 @@
 package edu.illinois.cs.cogcomp.saulexamples.nlp.SemanticRoleLabeling
 
-/** Created by Parisa on 1/5/16.
+/** Created by Parisa on 1/14/16.
   */
 
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames
@@ -13,26 +13,19 @@ import edu.illinois.cs.cogcomp.saulexamples.nlp.commonSensors._
 import scala.collection.JavaConversions._
 
 object pipelineApp extends App {
+  val useGoldPredicate = false
+  val useGoldArgBoundaries = false
   val trainPredicates = true
   val trainArgTypeWithGold = false
   val trainArgIdWithCandidates = false
   val trainArgTypeWithCandidates = false
-  val modelsDir = "models_aTr"
 
-  if (!trainArgTypeWithGold) {
+  if (!useGoldPredicate) {
     srlDataModel.sentencesToTokens.addSensor(textAnnotationToTokens _)
   }
   populateGraphwithGoldSRL(srlDataModel, srlDataModel.sentences)
 
-  if (trainArgTypeWithGold) {
-    // Here first train and test the argClassifier Given the ground truth Boundaries (i.e. no negative class).
-    argumentTypeLearner.setModelDir(modelsDir)
-    argumentTypeLearner.learn(100)
-    argumentTypeLearner.test()
-    argumentTypeLearner.save()
-  }
-
-  if (trainArgIdWithCandidates || trainArgTypeWithCandidates || trainPredicates) {
+  if (!useGoldPredicate) {
     val predicateTrainCandidates = tokens.getTrainingInstances.filter((x: Constituent) => posTag(x).startsWith("VB"))
       .map(c => c.cloneForNewView(ViewNames.SRL_VERB))
     val predicateTestCandidates = tokens.getTestingInstances.filter((x: Constituent) => posTag(x).startsWith("VB"))
@@ -45,7 +38,17 @@ object pipelineApp extends App {
 
     predicates.populate(negativePredicateTrain)
     predicates.populate(negativePredicateTest, train = false)
+  }
 
+  if (trainArgTypeWithGold && useGoldArgBoundaries) { //this block should be here before populating negative args below!!
+    //train and test the argClassifier Given the ground truth Boundaries (i.e. no negative class).
+    argumentTypeLearner.setModelDir("models_aTr")
+    argumentTypeLearner.learn(100)
+    argumentTypeLearner.test()
+    argumentTypeLearner.save()
+  }
+
+  if (!useGoldArgBoundaries) {
     val XuPalmerCandidateArgsTraining = predicates.getTrainingInstances.flatMap(x => xuPalmerCandidate(x, (sentences(x.getTextAnnotation) ~> sentencesToStringTree).head))
     val XuPalmerCandidateArgsTesting = predicates.getTestingInstances.flatMap(x => xuPalmerCandidate(x, (sentences(x.getTextAnnotation) ~> sentencesToStringTree).head))
 
@@ -56,33 +59,41 @@ object pipelineApp extends App {
 
     relations.populate(negativePalmerTrainCandidates)
     relations.populate(negativePalmerTestCandidates, train = false)
-    println("all relations number after population:" + srlDataModel.relations().size)
 
-    if (trainPredicates) {
-      predicateClassifier.setModelDir("models_dTr")
-      println("Training predicate identifier")
-      predicateClassifier.learn(100)
-      predicateClassifier.save()
-      print("isPredicate test results:")
-      predicateClassifier.test()
-    }
+  }
+  println("all relations number after population:" + srlDataModel.relations().size)
+  if (trainPredicates) {
+    predicateClassifier.setModelDir("models_dTr")
+    println("Training predicate identifier")
+    predicateClassifier.learn(100, predicates.trainingSet)
+    predicateClassifier.save()
+    print("isPredicate test results:")
+    predicateClassifier.test(predicates.testingSet)
+  }
 
-    if (trainArgIdWithCandidates) {
-      argumentXuIdentifierGivenApredicate.setModelDir("models_bTr")
-      println("Training argument identifier")
-      argumentXuIdentifierGivenApredicate.learn(100)
-      print("isArgument test results:")
-      argumentXuIdentifierGivenApredicate.test()
-      argumentXuIdentifierGivenApredicate.save()
-    }
+  if (trainArgIdWithCandidates && useGoldPredicate) {
+    argumentXuIdentifierGivenApredicate.setModelDir("models_bTr")
+    println("Training argument identifier")
+    argumentXuIdentifierGivenApredicate.learn(100)
+    print("isArgument test results:")
+    argumentXuIdentifierGivenApredicate.test()
+    argumentXuIdentifierGivenApredicate.save()
+  }
 
-    if (trainArgTypeWithCandidates) {
-      argumentTypeLearner.setModelDir("models_cTr")
-      println("Training argument classifier")
-      argumentTypeLearner.learn(100)
-      print("argument classifier test results:")
-      argumentTypeLearner.test()
-      argumentTypeLearner.save()
-    }
+  if (trainArgIdWithCandidates && !useGoldPredicate) {
+    argumentXuIdentifierGivenApredicate.setModelDir("models_eTr")
+    println("Training argument identifier")
+    argumentXuIdentifierGivenApredicate.learn(100)
+    print("isArgument test results:")
+    argumentXuIdentifierGivenApredicate.test()
+    argumentXuIdentifierGivenApredicate.save()
+  }
+  if (trainArgTypeWithCandidates && !useGoldPredicate) {
+    argumentTypeLearner.setModelDir("models_fTr")
+    println("Training argument classifier")
+    argumentTypeLearner.learn(100)
+    print("argument classifier test results:")
+    argumentTypeLearner.test()
+    argumentTypeLearner.save()
   }
 }
