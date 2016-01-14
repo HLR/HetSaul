9a5165eb5f235bea59aea18a9158bfac23eef485
xxu52 <xxu52@mir.cs.illinois.edu>
2016-01-14 09:58:30 -0600

    Refactor compilation code

--- saul-webapp/app/util/runtimeCompiler.scala
+++ saul-webapp/app/util/runtimeCompiler.scala
@@ -1,21 +1,22 @@
-import scala.tools.nsc.{Global, Settings}
+import scala.tools.nsc.{ Global, Settings }
 import java.io._
 
-object runtimeCompiler{
+object runtimeCompiler {
 
-    def main(args: Array[String]): Unit = {
-  val global = new Global(new Settings()) 
+  def main(args: Array[String]): Unit = {
+    val global = new Global(new Settings())
 
-  val compiler = new global.Run  
+    val compiler = new global.Run
 
-  compiler.compile(List("spamDataModel.scala","Document.java","DocumentReader.java"))  // invoke compiler. it creates Test.class.
+    compiler.compile(List("spamDataModel.scala", "Document.java", "DocumentReader.java")) // invoke compiler. it creates Test.class.
 
-  val classLoader = new java.net.URLClassLoader(
-    Array(new File(".").toURI.toURL),  // Using current directory.
-    this.getClass.getClassLoader)
+    val classLoader = new java.net.URLClassLoader(
+      Array(new File(".").toURI.toURL), // Using current directory.
+      this.getClass.getClassLoader
+    )
 
-  val clazz = classLoader.loadClass("Test") // load class 
+    val clazz = classLoader.loadClass("Test") // load class 
 
-  clazz.newInstance  // create an instance, which will print Hello World.
-}
+    clazz.newInstance // create an instance, which will print Hello World.
+  }
 }
