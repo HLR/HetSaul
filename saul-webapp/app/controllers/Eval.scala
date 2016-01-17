250d6765eb08f64a70bdd3b3895fcfd5bce7d75c
xxu52 <xxu52@mir.cs.illinois.edu>
2016-01-17 10:31:41 -0600

    Add barebone eval interface

--- saul-webapp/app/controllers/Eval.scala
+++ saul-webapp/app/controllers/Eval.scala
@@ -1,5 +1,17 @@
 package controllers
 
+import play.api.libs.json.{ Json, JsValue }
+
+//TODO: define eval function in object or class?
+object Eval {
+
+  //@Joe in case you also need the full source code I also pass
+  // it in now, if it is not used I will refactor it later
+  def eval(scalaInstances: Iterable[Any], sourceCode: String): JsValue = {
+    Json.toJson("Error")
+  }
+}
+
 class Eval {
 
 }
