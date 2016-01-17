03f798c3a9dff0f15f66fa57823ee5a1913d845e
xxu52 <xxu52@mir.cs.illinois.edu>
2016-01-17 11:16:22 -0600

    Extract standalone compiler

--- saul-webapp/app/controllers/Application.scala
+++ saul-webapp/app/controllers/Application.scala
@@ -1,35 +1,26 @@
 package controllers
 
 import controllers.Event._
-import util.reflectUtils._
+
+import edu.illinois.cs.cogcomp.saul.datamodel.{ DataModel, dataModelJsonInterface }
 
 import play.api.mvc._
 import play.api.libs.json.{ JsValue, JsObject, Json }
-import io.Source
-import edu.illinois.cs.cogcomp.saul.datamodel.{ DataModel, dataModelJsonInterface }
+
 import java.io.File
-import java.nio.file.Files
-import javax.tools._
 
 import scala.collection.mutable
-import java.net.{ URLClassLoader, URL }
-import java.nio.charset.StandardCharsets
-import util.classExecutor
-
-import scala.collection.JavaConverters._
-import java.lang.reflect.Method
-import scala.tools.nsc.{ Global, Settings }
 import scala.reflect.internal.util.{ BatchSourceFile, Position }
-
-import scala.reflect.runtime.universe
+import scala.tools.nsc.Settings
 import scala.tools.nsc.reporters.{ Reporter, AbstractReporter }
 
-class Application extends Controller {
+import util.reflectUtils._
+import util.classExecutor
 
-  //val saulExternalLibs = new File(classPathOfClass("edu.illinois.cs.cogcomp.lbjava.parse.Parser")(0)).getParentFile().getParentFile().getParentFile().getPath()
-  //val resolvedSaulExternalLibs = if(saulExternalLibs.endsWith(File.separator)) (saulExternalLibs+"*") else (saulExternalLibs + File.separator + "*")
+object Application {
+
   val rootDir = "/tmp"
-  val root: File = new File(rootDir); // On Windows running on C:\, this is C:\java.
+
   val completeClasspath = (List(
     "scala.tools.nsc.Interpreter",
     "scala.AnyVal",
@@ -40,8 +31,15 @@ class Application extends Controller {
     "edu.illinois.cs.cogcomp.curator.CuratorFactory"
   ).flatMap(x => classPathOfClass(x)) ::: List(rootDir)).mkString(File.pathSeparator)
 
-  val rootURL = root.toURI.toURL
-  val classLoader = URLClassLoader.newInstance(Array(rootURL), this.getClass().getClassLoader());
+}
+
+class Application extends Controller {
+
+  import Application._
+
+  val compiler = new Compiler(rootDir, completeClasspath, getScalaCompilerReporter)
+  //val saulExternalLibs = new File(classPathOfClass("edu.illinois.cs.cogcomp.lbjava.parse.Parser")(0)).getParentFile().getParentFile().getParentFile().getPath()
+  //val resolvedSaulExternalLibs = if(saulExternalLibs.endsWith(File.separator)) (saulExternalLibs+"*") else (saulExternalLibs + File.separator + "*")
 
   def index = Action { implicit request =>
     Ok(views.html.main("Your new application is ready."))
@@ -134,10 +132,10 @@ class Application extends Controller {
       case (k, _) => k contains ".java"
     }
 
-    compileJava(javaFiles)
+    compiler.compileJava(javaFiles)
 
     try {
-      Left(compileScala(scalaFiles))
+      Left(compiler.compileScala(scalaFiles))
     } catch {
       case errors: CompilerException => Right(getErrorJson(Json.toJson(errors.messages)))
       case _ => Right(Json.toJson("Unknown exception."))
@@ -184,96 +182,8 @@ class Application extends Controller {
     }
   }
 
-  def compileScala(files: Map[String, String]): Iterable[Any] = files map {
+}
 
-    case (name, code) => {
-
-      play.api.Logger.info("Compiling Scala code.")
-
-      val sourceFiles = files map { x: (String, String) =>
-        x match {
-          case (k, v) => new BatchSourceFile("(inline)", v)
-        }
-      }
-
-      val sett = new Settings()
-      sett.classpath.value = completeClasspath
-      sett.bootclasspath.value = sett.classpath.value
-      sett.outdir.value = "/tmp"
-
-      val reporter = getScalaCompilerReporter(sett)
-      val g = new Global(sett, reporter)
-      val run = new g.Run
-      run.compileSources(sourceFiles.toList)
-
-      //Ignore warnings for now
-      if (reporter.hasErrors /*|| reporter.WARNING.count > 0*/ ) {
-        val msgs: List[List[String]] = reporter match {
-          case collector: MessageCollector =>
-            collector.messages.toList
-          case _ =>
-            List(List(reporter.toString))
-        }
-        throw new CompilerException(msgs)
-      }
-      val clazz = instantiateClass(name, getCodePackageName(code))
-
-      clazz
-    }
-
-  }
-
-  def compileJava(files: Map[String, String]): Unit = {
-    play.api.Logger.info("Compiling Java code.")
-
-    val names = files map {
-      case (k, v) => {
-        // Save source in .java file.
-        val filePath = getCodePackageName(v).replaceAll("\\.", "/") + "/" + k
-        val sourceFile: File = new File(root, filePath);
-        sourceFile.getParentFile().mkdirs();
-        Files.write(sourceFile.toPath(), v.split("\\n").toList.asJava, StandardCharsets.UTF_8);
-        sourceFile.getPath()
-      }
-    } toList
-
-    if (names.isEmpty) return
-
-    val names2 = List("-classpath") ::: List(completeClasspath) ::: names
-
-    // Compile source file.
-    val compiler: JavaCompiler = ToolProvider.getSystemJavaCompiler();
-    compiler.run(null, null, null, names2: _*);
-
-  }
-
-  def getCurrentClasspath() = {
-    val getDeclaredMethod: Method = new URLClassLoader(Array(rootURL)).getClass().getDeclaredMethod("getURLs")
-    getDeclaredMethod.setAccessible(true);
-    val result = getDeclaredMethod.invoke(ClassLoader.getSystemClassLoader()).asInstanceOf[Array[URL]]
-    result
-  }
-
-  def instantiateClass(fileName: String, packageName: String) = {
-    // Load and instantiate compiled class.
-    val name = fileName.split('.')
-    val runtimeMirror = universe.runtimeMirror(classLoader)
-    //val clazz = classLoader.loadClass(packageName + "." + name(0))
-    //val constructor = clazz.getConstructor()
-    //constructor.setAccessible(true)
-    //val instance = constructor.newInstance()
-    val module = runtimeMirror.staticModule(packageName + "." + name(0))
-
-    val obj = runtimeMirror.reflectModule(module)
-
-    obj.instance
-  }
-
-  trait MessageCollector {
-    val messages: Seq[List[String]]
-  }
-  class CompilerException(val messages: List[List[String]]) extends Exception(
-    "Compiler exception " + messages.map(_.mkString("\n")).mkString("\n")
-  )
-
+trait MessageCollector {
+  val messages: Seq[List[String]]
 }
