5cb43fb6a98a43f9d097459ea5943910b3840ffd
xxu52 <xxu52@mir.cs.illinois.edu>
2016-01-17 15:45:54 -0600

    Add frontend elements for visualization

--- saul-webapp/app/controllers/Eval.scala
+++ saul-webapp/app/controllers/Eval.scala
@@ -5,9 +5,10 @@ import play.api.libs.json.{ Json, JsValue }
 //TODO: define eval function in object or class?
 object Eval {
 
-  //@Joe in case you also need the full source code I also pass
-  // it in now, if it is not used I will refactor it later
-  def eval(scalaInstances: Iterable[Any], sourceCode: String): JsValue = {
+  //TODO: implement this
+  //@Joe in case you also need the full source code
+  //and compiler I also pass it in. If not used remove it later
+  def eval(scalaInstances: Iterable[Any], fileMap: Map[String, String], compiler: Compiler): JsValue = {
     Json.toJson("Error")
   }
 }
