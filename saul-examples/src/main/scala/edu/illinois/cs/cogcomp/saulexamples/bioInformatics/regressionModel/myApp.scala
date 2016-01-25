61114a50c505b9ff1e04589fc5f1d8d6e809561e
kordjam <kordjam@illinois.edu>
2016-01-25 12:59:06 -0600

    added some queries

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/bioInformatics/regressionModel/myApp.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/bioInformatics/regressionModel/myApp.scala
@@ -1,8 +1,9 @@
 package edu.illinois.cs.cogcomp.saulexamples.bioInformatics.regressionModel
 
+import edu.illinois.cs.cogcomp.saul.datamodel.node.Path
 import edu.illinois.cs.cogcomp.saulexamples.bioInformatics.regressionModel.Classifiers.dResponseClassifier
 import edu.illinois.cs.cogcomp.saulexamples.bioInformatics.regressionModel.knowEngDataModel._
-import edu.illinois.cs.cogcomp.saulexamples.bioInformatics.{ Edges, Sample_Reader, drugExampleReader }
+import edu.illinois.cs.cogcomp.saulexamples.bioInformatics.{Gene, Edges, Sample_Reader, drugExampleReader}
 
 import scala.collection.JavaConversions._
 /** Created by Parisa on 6/25/15.
@@ -11,11 +12,11 @@ object myApp {
 
   def main(args: Array[String]): Unit = {
 
-    val patients_data = new Sample_Reader("./data/biology/individual_samples.txt").patientCollection.slice(0, 10)
-    val patient_drug_data = new drugExampleReader().pdReader("./data/biology/auc_response.txt").filter(x => x.drugId == "D_0").slice(0, 10)
-    val GCollection = new Edges("./data/biology/edgesGG.txt").geneCollection
-    val patient_gene_data = new drugExampleReader().pgReader("./data/biology/gene2med_probe_expr.txt").filter((x => GCollection.exists(y => (y.GeneID.equals(x.Gene_ID))))).slice(0, 10)
-    val GGCollection = new Edges("./data/biology/edgesGG.txt").edgeCollection.slice(0, 30)
+    val patients_data = new Sample_Reader("./data/biology/individual_samples.txt").patientCollection.slice(0, 5)
+    val patient_drug_data = new drugExampleReader().pdReader("./data/biology/auc_response.txt").filter(x => x.drugId == "D_0").slice(0, 5)
+    val GCollection = new Edges("./data/biology/edgesGG.txt").geneCollection.slice(0,10)
+    val patient_gene_data = new drugExampleReader().pgReader("./data/biology/gene2med_probe_expr.txt").filter((x => GCollection.exists(y => (y.GeneID.equals(x.Gene_ID))))).slice(0, 5)
+    val GGCollection = new Edges("./data/biology/edgesGG.txt").edgeCollection.slice(0, 20)
 
     patients.populate(patients_data)
     patientDrug.populate(patient_drug_data)
@@ -23,15 +24,18 @@ object myApp {
     genes.populate(GCollection)
     geneGene.populate(GGCollection)
 
-    patients() ~> -pdPatient
-    // patients() ~>
+    patientDrug().filter(x=> drugResponse(x)>12)
 
-    //    Si.genes.names.filter(logical expression on gene.value).foreach.getNeighborhood.aggregate().zipidex()
-    //    %Si.genses.names.filter(logical expression on gene.value).getgenes.findBestmaching(a list of pathways,k-best)
-    //    %G.foreach.getall(logical expresion).sum.filter(logical ).zipwithindex
+    (patients() ~> -pgPatient ~> pgGenes) prop gene_KEGG
+
+    genes(genes().head)~> -geneGenes prop textSimilarity
+
+    genes(Path.findPath(genes().head,genes, genes().head).asInstanceOf[Seq[Gene]]) prop gene_GoTerm
+    //.filter(x=> gene_GoTerm(x.asInstanceOf[Gene]).equals("Go1"))
 
     dResponseClassifier.learn(1)
-    print("finished learning.")
+
     dResponseClassifier.testContinuos(patient_drug_data)
+
   }
 }
