beaed882bc6b0d4d0d9b6b58894766c1746688d4
kordjam <kordjam@illinois.edu>
2016-02-19 10:27:18 -0600

    cleaned the comments

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/entityRelationClassifiers.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/entityRelationClassifiers.scala
@@ -5,16 +5,15 @@ import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 import edu.illinois.cs.cogcomp.saul.constraint.ConstraintTypeConversion._
 import edu.illinois.cs.cogcomp.saul.datamodel.property.Property
 import edu.illinois.cs.cogcomp.saulexamples.EntityMentionRelation.datastruct.{ ConllRawSentence, ConllRawToken, ConllRelation }
-import EntityRelationDataModel._
+import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.EntityRelationDataModel._
+
 object EntityRelationClassifiers {
 
   object orgClassifier extends Learnable[ConllRawToken](EntityRelationDataModel) {
     def label: Property[ConllRawToken] = entityType is "Org"
     override lazy val classifier = new SparseNetworkLearner()
-    //override def feature = using(
-    //word,phrase,containsSubPhraseMent,containsSubPhraseIng,
-    // containsInPersonList,wordLen,containsInCityList
-    // )
+    override def feature = using(windowWithIn[ConllRawSentence](-2, 2, List(pos)), word, phrase, containsSubPhraseMent, containsSubPhraseIng,
+      containsInPersonList, wordLen, containsInCityList)
   }
 
   object personClassifier extends Learnable[ConllRawToken](EntityRelationDataModel) {
@@ -26,20 +25,14 @@ object EntityRelationClassifiers {
 
   object locationClassifier extends Learnable[ConllRawToken](EntityRelationDataModel) {
     def label: Property[ConllRawToken] = entityType is "Loc"
-    override def feature = using(
-      windowWithIn[ConllRawSentence](-2, 2, List(
-        pos
-      )), word, phrase, containsSubPhraseMent, containsSubPhraseIng,
-      containsInPersonList, wordLen, containsInCityList
-    )
+    override def feature = using(windowWithIn[ConllRawSentence](-2, 2, List(pos)), word, phrase, containsSubPhraseMent, containsSubPhraseIng,
+      containsInPersonList, wordLen, containsInCityList)
     override lazy val classifier = new SparseNetworkLearner()
   }
 
   object worksForClassifier extends Learnable[ConllRelation](EntityRelationDataModel) {
     def label: Property[ConllRelation] = relationType is "Work_For"
-    override def feature = using(
-      relFeature, relPos //,ePipe
-    )
+    override def feature = using(relFeature, relPos)
     override lazy val classifier = new SparseNetworkLearner()
   }
 
@@ -52,17 +45,13 @@ object EntityRelationClassifiers {
   }
   object LivesInClassifierPipe extends Learnable[ConllRelation](EntityRelationDataModel) {
     override def label: Property[ConllRelation] = relationType is "Live_In"
-    override def feature = using(
-      relFeature, relPos, ePipe
-    )
+    override def feature = using(relFeature, relPos, ePipe)
     override lazy val classifier = new SparseNetworkLearner()
   }
 
   object livesInClassifier extends Learnable[ConllRelation](EntityRelationDataModel) {
     def label: Property[ConllRelation] = relationType is "Live_In"
-    override def feature = using(
-      relFeature, relPos
-    )
+    override def feature = using(relFeature, relPos)
     override lazy val classifier = new SparseNetworkLearner()
   }
 
