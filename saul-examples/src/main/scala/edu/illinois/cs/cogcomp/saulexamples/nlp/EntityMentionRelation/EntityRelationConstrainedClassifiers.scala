2eeb2d7e72502fb3c5454310f8334dc22d70400b
kordjam <kordjam@illinois.edu>
2016-02-19 21:10:27 -0600

    splited constraints and enhanced naming

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/EntityRelationConstrainedClassifiers.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityMentionRelation/EntityRelationConstrainedClassifiers.scala
@@ -9,31 +9,31 @@ import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityMentionRelation.EntityRela
 object EntityRelationConstrainedClassifiers {
 
   object orgConstraintClassifier extends ConstrainedClassifier[ConllRawToken, ConllRelation](EntityRelationDataModel, orgClassifier) {
-    def subjectTo = Per_Org
+    def subjectTo = relationArgumentConstraints
     override val pathToHead = Some(-EntityRelationDataModel.pairTo2ndArg)
     override def filter(t: ConllRawToken, h: ConllRelation): Boolean = t.wordId == h.wordId1
   }
 
   object perConstraintClassifier extends ConstrainedClassifier[ConllRawToken, ConllRelation](EntityRelationDataModel, personClassifier) {
 
-    def subjectTo = Per_Org
+    def subjectTo = relationArgumentConstraints
     override val pathToHead = Some(-EntityRelationDataModel.pairTo1stArg)
     override def filter(t: ConllRawToken, h: ConllRelation): Boolean = t.wordId == h.wordId2
   }
 
   object locConstraintClassifier extends ConstrainedClassifier[ConllRawToken, ConllRelation](EntityRelationDataModel, locationClassifier) {
 
-    def subjectTo = Per_Org
+    def subjectTo = relationArgumentConstraints
     override val pathToHead = Some(-EntityRelationDataModel.pairTo2ndArg)
     //TODO add test unit for this filter
     override def filter(t: ConllRawToken, h: ConllRelation): Boolean = t.wordId == h.wordId2
   }
 
   object work_P_O_relationClassifier extends ConstrainedClassifier[ConllRelation, ConllRelation](EntityRelationDataModel, worksForClassifier) {
-    def subjectTo = Per_Org
+    def subjectTo = relationArgumentConstraints
   }
 
   object liveIn_P_O_relationClassifier extends ConstrainedClassifier[ConllRelation, ConllRelation](EntityRelationDataModel, livesInClassifier) {
-    def subjectTo = Per_Org
+    def subjectTo = relationArgumentConstraints
   }
 }
