36e9009c75a35701c93c33530fc5a9c4375a4a85
kordjam <kordjam@illinois.edu>
2016-03-02 09:02:41 -0600

    remove var

--- saul-core/src/main/scala/edu/illinois/cs/cogcomp/saul/datamodel/property/PairwiseConjunction.scala
+++ saul-core/src/main/scala/edu/illinois/cs/cogcomp/saul/datamodel/property/PairwiseConjunction.scala
@@ -6,9 +6,18 @@ object PairwiseConjunction {
     var a: List[String] = List()
     for (t1 <- 0 until x.size - 1) {
       for (t2 <- t1 + 1 until x.size) {
-        a = (x(t1).name + "_" + x(t1)(y) + "_" + x(t2).name + "_" + x(t2)(y)) :: a }
+        a = (x(t1).name + "_" + x(t1)(y) + "_" + x(t2).name + "_" + x(t2)(y)) :: a
+      }
     }
     a
   }
-}
 
+  def concat(x: String, l: List[String]): List[String] = {
+    l.map(t=> x+ t )
+  }
+}
+//x.size match {
+//case 0 => List()
+//case 1 => List(x.mkString)
+//case _ => concat(x.head.name + "_" + x.head(y) + "_"), x.tail) :: apply(x.tail, y)
+//}
