c802dad560e42a5190166beb1abcbae5ccb20117
kordjam <kordjam@illinois.edu>
2016-03-03 12:19:36 -0600

    - pairwiseConcat using recursion avoid using mutable

--- saul-core/src/main/scala/edu/illinois/cs/cogcomp/saul/datamodel/property/PairwiseConjunction.scala
+++ saul-core/src/main/scala/edu/illinois/cs/cogcomp/saul/datamodel/property/PairwiseConjunction.scala
@@ -3,21 +3,14 @@ package edu.illinois.cs.cogcomp.saul.datamodel.property
 object PairwiseConjunction {
 
   def apply[T](x: List[Property[T]], y: T): List[String] = {
-    var a: List[String] = List()
-    for (t1 <- 0 until x.size - 1) {
-      for (t2 <- t1 + 1 until x.size) {
-        a = (x(t1).name + "_" + x(t1)(y) + "_" + x(t2).name + "_" + x(t2)(y)) :: a
-      }
-    }
-    a
+    concat(x.head, x.drop(1), y)
   }
 
-  def concat(x: String, l: List[String]): List[String] = {
-    l.map(t=> x+ t )
-  }
+  def concat[T](a: Property[T], b: List[Property[T]], y: T): List[String] =
+    {
+      if (b.isEmpty)
+        return List()
+      else
+        return b.map(x => a.name + "_" + a(y) + "_" + x.name + "_" + x(y)) ::: concat(b.head, b.drop(1), y)
+    }
 }
-//x.size match {
-//case 0 => List()
-//case 1 => List(x.mkString)
-//case _ => concat(x.head.name + "_" + x.head(y) + "_"), x.tail) :: apply(x.tail, y)
-//}
