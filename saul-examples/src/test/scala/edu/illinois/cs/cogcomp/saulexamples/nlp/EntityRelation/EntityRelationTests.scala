92020b99d0606622e7c394e054283ef2a26a6a5e
khashab2 <khashab2@illinois.edu>
2016-04-05 01:34:55 -0500

    minor fix in EntityRelationTest

--- saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityRelation/EntityRelationTests.scala
+++ saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityRelation/EntityRelationTests.scala
@@ -10,20 +10,20 @@ class EntityRelationTests extends FlatSpec with Matchers {
   "entity classifier " should " should work. " in {
     EntityRelationClassifiers.loadIndependentEntityModels()
     val scores = PersonClassifier.test() ++ OrganizationClassifier.test() ++ LocationClassifier.test()
-    scores.foreach { case (label, score) => (score.f1 > minScore) should be(true) }
+    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
   }
 
   "independent relation classifier " should " should work. " in {
     EntityRelationClassifiers.loadIndependentRelationModels()
     val scores = WorksForClassifier.test() ++ LivesInClassifier.test() ++
       LocatedInClassifier.test() ++ OrgBasedInClassifier.test()
-    scores.foreach { case (label, score) => (score.f1 > minScore) should be(true) }
+    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
   }
 
   "pipeline relation classifiers " should " should work. " in {
     EntityRelationClassifiers.loadIndependentEntityModels()
     EntityRelationClassifiers.loadPipelineRelationModels()
     val scores = WorksForClassifierPipeline.test() ++ LivesInClassifierPipeline.test()
-    scores.foreach { case (label, score) => (score.f1 > minScore) should be(true) }
+    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
   }
 }
