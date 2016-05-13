b3bfc28b20fe86159b170dc952fa6cc8bf9d8b49
khashab2 <khashab2@illinois.edu>
2016-05-13 15:07:21 -0500

    fix ER test as a result of changes in learnable.

--- saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityRelation/EntityRelationTests.scala
+++ saul-examples/src/test/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EntityRelation/EntityRelationTests.scala
@@ -2,7 +2,8 @@ package edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation
 
 import edu.illinois.cs.cogcomp.saul.classifier.ClassifierUtils
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation.EntityRelationClassifiers._
-import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation.EntityRelationConstrainedClassifiers.{ WorksFor_PerOrg_ConstrainedClassifier, OrgConstrainedClassifier, PerConstrainedClassifier }
+import edu.illinois.cs.cogcomp.saulexamples.nlp.EntityRelation.EntityRelationConstrainedClassifiers._
+
 import org.scalatest._
 
 class EntityRelationTests extends FlatSpec with Matchers {
@@ -15,7 +16,7 @@ class EntityRelationTests extends FlatSpec with Matchers {
       PersonClassifier, OrganizationClassifier, LocationClassifier
     )
     val scores = PersonClassifier.test() ++ OrganizationClassifier.test() ++ LocationClassifier.test()
-    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
+    scores.foreach { case score => (score.f1 > minScore) should be(true) }
   }
 
   "independent relation classifier " should " should work. " in {
@@ -25,7 +26,7 @@ class EntityRelationTests extends FlatSpec with Matchers {
     )
     val scores = WorksForClassifier.test() ++ LivesInClassifier.test() ++
       LocatedInClassifier.test() ++ OrgBasedInClassifier.test()
-    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
+    scores.foreach { case score => (score.f1 > minScore) should be(true) }
   }
 
   "pipeline relation classifiers " should " should work. " in {
@@ -35,7 +36,7 @@ class EntityRelationTests extends FlatSpec with Matchers {
       WorksForClassifierPipeline, LivesInClassifierPipeline
     )
     val scores = WorksForClassifierPipeline.test() ++ LivesInClassifierPipeline.test()
-    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
+    scores.foreach { case score => (score.f1 > minScore) should be(true) }
   }
 
   "L+I entity-relation classifiers " should " should work. " in {
@@ -45,6 +46,6 @@ class EntityRelationTests extends FlatSpec with Matchers {
       WorksForClassifier, LivesInClassifier, LocatedInClassifier, OrgBasedInClassifier
     )
     val scores = PerConstrainedClassifier.test() ++ WorksFor_PerOrg_ConstrainedClassifier.test()
-    scores.foreach { case (label, score) => (score._1 > minScore) should be(true) }
+    scores.foreach { case score => (score.f1 > minScore) should be(true) }
   }
 }
