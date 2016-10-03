ce7a9eb367bf9854bc842b139e27bd93b6eb3d30
Taher Rahgooy <Rahgooy@users.noreply.github.com>
2016-10-03 23:58:42 +0330

    saul-weka-wrapper (#407)
    * Saul wrapper for weka added
    
    * Binary representation used for discrete values.
    
    * Review comments addressed.

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/spamClassifiers.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/EmailSpam/spamClassifiers.scala
@@ -8,8 +8,10 @@ package edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam
 
 import edu.illinois.cs.cogcomp.lbjava.learn.SupportVectorMachine
 import edu.illinois.cs.cogcomp.saul.classifier.Learnable
+import edu.illinois.cs.cogcomp.saul.learn.SaulWekaWrapper
 import edu.illinois.cs.cogcomp.saulexamples.data.Document
 import edu.illinois.cs.cogcomp.saulexamples.nlp.EmailSpam.SpamDataModel._
+import weka.classifiers.bayes.NaiveBayes
 
 object SpamClassifiers {
   object SpamClassifier extends Learnable[Document](docs) {
@@ -28,6 +30,11 @@ object SpamClassifiers {
   object DeserializedSpamClassifier extends Learnable[Document](docs) {
     def label = spamLabel
     override lazy val classifier = new SupportVectorMachine()
+    override def feature = using(wordFeature)
+  }
+  object SpamClassifierWeka extends Learnable[Document](docs) {
+    def label = spamLabel
+    override lazy val classifier = new SaulWekaWrapper(new NaiveBayes())
     override def feature = using(wordFeature)
   }
 }
