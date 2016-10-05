2e19cc5dc3594a091df4fd70e0e121b526e10faa
kordjam <kordjam@illinois.edu>
2016-10-04 23:49:46 -0500

    format

--- saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/TwitterSentimentAnalysis/twitterSentimentApp.scala
+++ saul-examples/src/main/scala/edu/illinois/cs/cogcomp/saulexamples/nlp/TwitterSentimentAnalysis/twitterSentimentApp.scala
@@ -4,7 +4,7 @@ import java.util
 
 import com.twitter.hbc.core.endpoint.Location
 import edu.illinois.cs.cogcomp.saulexamples.nlp.TwitterSentimentAnalysis.twitterClassifiers.sentimentClassifier
-import edu.illinois.cs.cogcomp.saulexamples.twitter.tweet.{ClassifierMessageHandler, Locations, TwitterClient}
+import edu.illinois.cs.cogcomp.saulexamples.twitter.tweet.{ ClassifierMessageHandler, Locations, TwitterClient }
 
 import scala.collection.JavaConversions._
 
