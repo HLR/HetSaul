a1685b06a55f5fee7d43aefa6e070ce1e7da76c1
kordjam <kordjam@illinois.edu>
2016-10-15 23:43:08 -0500

    svm->sparsenetwork

--- saul-core/src/test/scala/ssvm/randomclassifiers.scala
+++ saul-core/src/test/scala/ssvm/randomclassifiers.scala
@@ -1,6 +1,6 @@
 package ssvm
 
-import edu.illinois.cs.cogcomp.lbjava.learn.SupportVectorMachine
+import edu.illinois.cs.cogcomp.lbjava.learn.SparseNetworkLearner
 import edu.illinois.cs.cogcomp.saul.classifier.Learnable
 
 /** Created by Parisa on 10/14/16.
@@ -10,7 +10,7 @@ object randomClassifiers {
   object bClassifier extends Learnable[String](randomNode) {
     def label = randomLabel
     override def feature = using(randomProperty)
-    override lazy val classifier = new SupportVectorMachine()
+    override lazy val classifier = new SparseNetworkLearner()
     override val useCache = true
   }
 }
