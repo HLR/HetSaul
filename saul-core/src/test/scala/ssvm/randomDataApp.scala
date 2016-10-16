e76c37a799a9100c59689d087da8c951568c1936
kordjam <kordjam@illinois.edu>
2016-10-15 23:18:38 -0500

    -drawing the separator line and the weight vector

--- saul-core/src/test/scala/ssvm/randomDataApp.scala
+++ saul-core/src/test/scala/ssvm/randomDataApp.scala
@@ -18,108 +18,24 @@ object randomDataApp extends App {
   randomDataModel.write(graphCacheFile)
   bClassifier.learn(30)
   bClassifier.test(ex)
-
+  val w = bClassifier.classifier.getWeights
   val p2 = (randomNode() prop randomProperty) zip (randomNode() prop randomLabel)
   val x2 = DenseVector(p2.filter(x => x._2.equals("1")).map(x => x._1(0)).toArray)
   val y2 = DenseVector(p2.filter(x => x._2.equals("1")).map(x => x._1(1)).toArray)
   val x_minus = DenseVector(p2.filter(x => x._2.equals("-1")).map(x => x._1(0)).toArray)
   val y_minus = DenseVector(p2.filter(x => x._2.equals("-1")).map(x => x._1(1)).toArray)
 
-  // val p1=(randomNode() prop randomLabel)
-
   val f = Figure()
   val p = f.subplot(0)
+  p.ylim(-3.0, 3)
+  p.xlim(-3, 3)
   val x = linspace(0.0, 1.0)
   p += plot(x2, y2, '.')
   p += plot(x_minus, y_minus, '.')
+  p += plot(DenseVector(0, w(0)), DenseVector(0, w(1)))
+  p += plot(DenseVector(0, w(0)), DenseVector(0, w(1)))
+  p += plot(DenseVector(10 * w(1), (-10) * w(1)), DenseVector(-10 * w(0), 10 * w(0)))
   p.xlabel = "x axis"
   p.ylabel = "y axis"
   f.saveas("lines.png")
-
 }
-//function test_svm_struct_learn
-//% TEST_SVM_STRUCT_LEARN
-//%   A demo function for SVM_STRUCT_LEARN(). It shows how to use
-//%   SVM-struct to learn a standard linear SVM.
-//
-//randn('state',0) ;
-//rand('state',0) ;
-//
-//% ------------------------------------------------------------------
-//%                                                      Generate data
-//% ------------------------------------------------------------------
-//
-//th = pi/3 ;
-//c = cos(th) ;
-//s = sin(th) ;
-//
-//patterns = {} ;
-//labels = {} ;
-//for i=1:100
-//patterns{i} = diag([2 .5]) * randn(2, 1) ;
-//labels{i}   = 2*(randn > 0) - 1 ;
-//patterns{i}(2) = patterns{i}(2) + labels{i} ;
-//patterns{i} = [c -s ; s c] * patterns{i}  ;
-//end
-//
-//% ------------------------------------------------------------------
-//%                                                    Run SVM struct
-//% ------------------------------------------------------------------
-//
-//parm.patterns = patterns ;
-//parm.labels = labels ;
-//parm.lossFn = @lossCB ;
-//parm.constraintFn  = @constraintCB ;
-//parm.featureFn = @featureCB ;
-//parm.dimension = 2 ;
-//parm.verbose = 1 ;
-//model = svm_struct_learn(' -c 1.0 -o 1 -v 1 ', parm) ;
-//w = model.w ;
-//
-//% ------------------------------------------------------------------
-//%                                                              Plots
-//% ------------------------------------------------------------------
-//
-//figure(1) ; clf ; hold on ;
-//x = [patterns{:}] ;
-//y = [labels{:}] ;
-//plot(x(1, y>0), x(2,y>0), 'g.') ;
-//plot(x(1, y<0), x(2,y<0), 'r.') ;
-//set(line([0 w(1)], [0 w(2)]), 'color', 'y', 'linewidth', 4) ;
-//xlim([-3 3]) ;
-//ylim([-3 3]) ;
-//set(line(10*[w(2) -w(2)], 10*[-w(1) w(1)]), ...
-//'color', 'y', 'linewidth', 2, 'linestyle', '-') ;
-//axis equal ;
-//set(gca, 'color', 'b') ;
-//w
-//end
-//
-//% ------------------------------------------------------------------
-//%                                               SVM struct callbacks
-//% ------------------------------------------------------------------
-//
-//function delta = lossCB(param, y, ybar)
-//delta = double(y ~= ybar) ;
-//if param.verbose
-//fprintf('delta = loss(%3d, %3d) = %f\n', y, ybar, delta) ;
-//end
-//end
-//
-//function psi = featureCB(param, x, y)
-//psi = sparse(y*x/2) ;
-//if param.verbose
-//fprintf('w = psi([%8.3f,%8.3f], %3d) = [%8.3f, %8.3f]\n', ...
-//x, y, full(psi(1)), full(psi(2))) ;
-//end
-//end
-//
-//function yhat = constraintCB(param, model, x, y)
-//% slack resaling: argmax_y delta(yi, y) (1 + <psi(x,y), w> - <psi(x,yi), w>)
-//% margin rescaling: argmax_y delta(yi, y) + <psi(x,y), w>
-//if dot(y*x, model.w) > 1, yhat = y ; else yhat = - y ; end
-//if param.verbose
-//fprintf('yhat = violslack([%8.3f,%8.3f], [%8.3f,%8.3f], %3d) = %3d\n', ...
-//model.w, x, y, yhat) ;
-//end
-//end
